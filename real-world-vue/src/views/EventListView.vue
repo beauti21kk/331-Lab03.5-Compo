969a770d6b6fab2518f87597689b13b724afe2ba
Beautiiiii <95921996+Beautiiiii@users.noreply.github.com>
2023-08-05 18:07:30 +0700

    start 3.5

---
+++ real-world-vue/src/views/EventListView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import EventCard from '../components/EventCard.vue'
+import type { EventItem } from '@/type'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import eventService from '@/services/EventService'
+ 
+const events: Ref<Array<EventItem>> = ref([])
+  eventService.getEvent().then((response) => {
+    events.value = response.data
+  })
+  
+</script>
+
+<template>
+  <main class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+  </main>
+</template>
+
+<style scoped>
+.events{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}</style>
